15c3b3c3a1fbca06803d487ad3e188ed6fcc0d3c
rampemus <pasi.toivanen@live.com>
2019-05-22 22:48:14 +0300

    Personnel card updated to be more graphic

--- src/components/personnelCard.js
+++ src/components/personnelCard.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import Image from 'react-bootstrap/Image'
 import Popover from 'react-bootstrap/Popover'
 import OverlayTrigger from 'react-bootstrap/OverlayTrigger'
 import personnel from './personnel.json'
@@ -8,17 +9,27 @@ export default class PersonnelCard extends React.Component {
     constructor (props) {
         super(props)
 
+
     };
 
     render(){
-        const person = personnel.find(x => x.id === this.props.studentId)
+        /***
+            TODO: This query needs to be filtered in the back end!
+            So that students will not get all info
+            and teachers will not get administrator info
+        ***/
+        const person = personnel.find(student => student.id === this.props.studentId)
+        if (person.username.length < 1) {
+            person = personnel.find(student => student.username === this.props.studentId)
+        }
+
         return(
-            <OverlayTrigger trigger="focus" placement="right" overlay={(
+            <OverlayTrigger trigger="focus" placement={this.props.placement} overlay={(
               <Popover id="popover-basic" title={
                   person.firstNames.split(' ')[0] + ' ' + person.familyName + ' '
               }>
-                <p style={{textAlign:'center'}}>kuva</p>
-                <p><a href={"mailto:" + person.username + "@utu.fi"}>{person.username + "@utu.fi"}</a></p>
+                <p><Image src="user.png" style={{textAlign:'center'}} roundedCircle /></p>
+                <p style={{textAlign:'center'}}><a href={"mailto:" + person.username + "@utu.fi"}>{person.username + "@utu.fi"}</a></p>
               </Popover>
             )}>
                 <Button variant="link" style={{padding:'0px', margin:'0px'}}>{this.props.studentId}</Button>
